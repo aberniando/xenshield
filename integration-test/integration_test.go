546fa313a192fb9ba6c3a6462681c291026c5ab6
Achmad Berniando <achmad.berniando@mekari.com>
2024-11-20 07:40:11 +0700

    add more integration test coverage

--- integration-test/integration_test.go
+++ integration-test/integration_test.go
@@ -29,6 +29,103 @@ func GenerateRandomIP() string {
 	return strings.Join(octets, ".")
 }
 
+func TestHTTPFailedNoAPIKeyProvided(t *testing.T) {
+	ipAddress := GenerateRandomIP()
+
+	Test(t,
+		Description("FailedNoAPIKeyProvided"),
+		Get(fmt.Sprintf(`%s/transactions/%s`, basePath, ipAddress)),
+		Send().Headers("Content-Type").Add("application/json"),
+		Expect().Status().Equal(http.StatusForbidden),
+		Expect().Body().JSON().JQ(".message").Equal("Missing API key in request"),
+	)
+}
+
+func TestHTTPFailedInvalidAPIKey(t *testing.T) {
+	ipAddress := GenerateRandomIP()
+
+	Test(t,
+		Description("FailedNoAPIKeyProvided"),
+		Get(fmt.Sprintf(`%s/transactions/%s`, basePath, ipAddress)),
+		Send().Headers("Content-Type").Add("application/json"),
+		Send().Headers("api-key").Add("xxxxx"),
+		Expect().Status().Equal(http.StatusForbidden),
+		Expect().Body().JSON().JQ(".message").Equal("Invalid API key"),
+	)
+}
+
+func TestHTTPCreateTransactionWithoutMandatoryFields(t *testing.T) {
+	failureReason := "STOLEN_CARD"
+
+	body := fmt.Sprintf(`{
+		"failure_reason": "%s"
+	}`, failureReason)
+
+	Test(t,
+		Description("CreateTransactionWithoutMandatoryFields"),
+		Post(basePath+"/transactions"),
+		Send().Headers("Content-Type").Add("application/json"),
+		Send().Headers("api-key").Add("eGVuc2hpZWxk"),
+		Send().Body().String(body),
+		Expect().Status().Equal(http.StatusBadRequest),
+		Expect().Body().JSON().JQ(".message").Len().Equal(3),
+	)
+}
+
+func TestHTTPCreateTransactionInvalidBody1(t *testing.T) {
+	ipAddress := "xxxxx"
+	maskedCardNumber := fmt.Sprintf(`****-****-*********-%d`, rand.Intn(9000)+1000)
+	status := "SUCCESS"
+	failureReason := "INSUFFICIENT_BALANCE"
+
+	body := fmt.Sprintf(`{
+		"ip_address": "%s",
+		"masked_card_number": "%s",
+		"status": "%s",
+		"failure_reason": "%s"
+	}`, ipAddress, maskedCardNumber, status, failureReason)
+
+	Test(t,
+		Description("CreateTransactionInvalidBody1"),
+		Post(basePath+"/transactions"),
+		Send().Headers("Content-Type").Add("application/json"),
+		Send().Headers("api-key").Add("eGVuc2hpZWxk"),
+		Send().Body().String(body),
+		Expect().Status().Equal(http.StatusBadRequest),
+		Expect().Body().JSON().JQ(".message").Len().Equal(3),
+		Expect().Body().JSON().JQ(".message[0].message").Equal("invalid value format"),
+		Expect().Body().JSON().JQ(".message[1].message").Equal("invalid length"),
+		Expect().Body().JSON().JQ(".message[2].message").Equal("failure reason must be null on success transaction"),
+	)
+}
+
+func TestHTTPCreateTransactionInvalidBody2(t *testing.T) {
+	ipAddress := "xxxxx"
+	maskedCardNumber := fmt.Sprintf(`****-****-*********-%d`, rand.Intn(9000)+1000)
+	status := "FAILED"
+	failureReason := ""
+
+	body := fmt.Sprintf(`{
+		"ip_address": "%s",
+		"masked_card_number": "%s",
+		"status": "%s",
+		"failure_reason": "%s"
+	}`, ipAddress, maskedCardNumber, status, failureReason)
+
+	Test(t,
+		Description("CreateTransactionInvalidBody1"),
+		Post(basePath+"/transactions"),
+		Send().Headers("Content-Type").Add("application/json"),
+		Send().Headers("api-key").Add("eGVuc2hpZWxk"),
+		Send().Body().String(body),
+		Expect().Status().Equal(http.StatusBadRequest),
+		Expect().Body().JSON().JQ(".message").Len().Equal(3),
+		Expect().Body().JSON().JQ(".message[0].message").Equal("invalid value format"),
+		Expect().Body().JSON().JQ(".message[1].message").Equal("invalid length"),
+		Expect().Body().JSON().JQ(".message[2].message").Equal("failure reason must not be null on failed transaction"),
+	)
+}
+
 func TestHTTPCreateTransactionNotLinkedToStolenCard(t *testing.T) {
 	ipAddress := GenerateRandomIP()
 	maskedCardNumber := fmt.Sprintf(`****-****-****-%d`, rand.Intn(9000)+1000)
@@ -100,30 +197,5 @@ func TestHTTPCreateTransactionLinkedToStolenCard(t *testing.T) {
 		Send().Headers("api-key").Add("eGVuc2hpZWxk"),
 		Expect().Status().Equal(http.StatusOK),
 		Expect().Body().JSON().JQ(".linked_to_stolen_card").Equal(true),
-	)
-}
-
-func TestHTTPFailedNoAPIKeyProvided(t *testing.T) {
-	ipAddress := GenerateRandomIP()
-
-	Test(t,
-		Description("FailedNoAPIKeyProvided"),
-		Get(fmt.Sprintf(`%s/transactions/%s`, basePath, ipAddress)),
-		Send().Headers("Content-Type").Add("application/json"),
-		Expect().Status().Equal(http.StatusForbidden),
-		Expect().Body().JSON().JQ(".message").Equal("Missing API key in request"),
-	)
-}
-
-func TestHTTPFailedInvalidAPIKey(t *testing.T) {
-	ipAddress := GenerateRandomIP()
-
-	Test(t,
-		Description("FailedNoAPIKeyProvided"),
-		Get(fmt.Sprintf(`%s/transactions/%s`, basePath, ipAddress)),
-		Send().Headers("Content-Type").Add("application/json"),
-		Send().Headers("api-key").Add("xxxxx"),
-		Expect().Status().Equal(http.StatusForbidden),
-		Expect().Body().JSON().JQ(".message").Equal("Invalid API key"),
 	)
 }
